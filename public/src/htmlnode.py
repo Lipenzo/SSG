510924b6cac773bb529de762906c64003a0519d8
Lipenzo <192491525+Lipenzo@users.noreply.github.com>
2025-01-16 23:33:31 +0100

    created markdown_to_HTMLNode function which basicly crates whole page from a markdown

--- public/src/htmlnode.py
+++ public/src/htmlnode.py
@@ -16,15 +16,26 @@ class HTMLNode:
             props += f' {prop}="{self.props[prop]}"'
         return props
     
+    def __children_list(self):
+        repr = str(self.tag)
+        if self.children != None:
+            children = []
+            for child in self.children:
+                children.append(child.__children_list())
+            repr += "[" + " ".join(children) + "]"
+        return repr
+
     def __repr__(self):
         repr_string = "HTMLNode(\n"
         repr_string += f"    tag: {self.tag}\n"
         repr_string += f"    value: {self.value}\n"
         repr_string += "    children:"
-        if len(self.children) == 0:
+        if self.children == None or len(self.children) == 0:
             repr_string += " None"
-        for children in self.children:
-            repr_string += f" {children.tag}"
+        else:
+            for children in self.children:
+                repr_string += f" {children.__children_list()}"
         repr_string += "    props:"
         repr_string += self.props_to_html()
         repr_string += ")"
+        return repr_string

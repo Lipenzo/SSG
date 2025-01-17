7c84fc55e6e497b016b04fd0a72af6d65eeaac6c
Lipenzo <192491525+Lipenzo@users.noreply.github.com>
2025-01-17 01:53:41 +0100

    added public directory purging and populating it with static folders contents recursively

--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
-from textnode import *
+from os.path import exists, join, isfile
+from os import listdir, mkdir, remove
+from shutil import copy, rmtree
 
 def main():
-    txt_node = TextNode("text", TextType.BOLD, "https://www.boot.dev")
-    print(txt_node)
+    # need to clear public directory and then copy everything from static to public
+    destination = "public"
+    if not exists(destination):
+        mkdir(destination)
+    else:
+        if isfile(destination):
+            remove(destination)
+        else:
+            rmtree(destination)
+        mkdir(destination)
+    path = "static"
+    recursive_copy(destination, path)
+    
+def recursive_copy(destination, current_folder):
+    files = listdir(current_folder)
+    for file in files:
+        path = join(current_folder, file)
+        if isfile(path):
+            copy(path, destination)
+            print(f"file copied: {join(current_folder, file)}  ->  {join(destination, file)}")
+        else:
+            new_destination = join(destination, file)
+            new_path = join(current_folder, file)
+            mkdir(new_destination)
+            print(f"directory made: {new_destination}")
+            recursive_copy(new_destination, new_path)
 
 main()

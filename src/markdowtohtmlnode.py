ff65a46e0d82949e8fc6e40526804bbbdda9d18c
Lipenzo <192491525+Lipenzo@users.noreply.github.com>
2025-01-19 06:13:15 +0100

    inline markdown conversion for heaader and lists

--- src/markdowtohtmlnode.py
+++ src/markdowtohtmlnode.py
@@ -3,11 +3,21 @@ from leafnode import LeafNode
 from block import BlockType, block_to_html_type
 from utilities import markdown_to_blocks, text_to_text_nodes, text_to_node_html_node
 
+def __get_children(block):
+    leafnode_list = []
+    nodes = text_to_text_nodes(block)
+    for node in nodes:
+        leafnode_list.append(text_to_node_html_node(node))
+    return leafnode_list
 
 def heading_block_to_leafnode(block):
-    heading_level = len(block.split()[0])
+    heading_level = f"h{len(block.split()[0])}"
     text = block.lstrip("#")[1:]
-    return LeafNode(f"h{heading_level}",text)
+    children = __get_children(text)
+    if len(children) == 1 and children[0].tag == None:
+        return LeafNode(heading_level, children[0].value)
+    return HTMLNode(heading_level, children=children)
+    
 
 def code_block_to_leafnode(block):
     return LeafNode("code", block[3:-3])
@@ -22,21 +32,26 @@ def unorlist_block_to_HTMLNode(block):
     leafnode_list = []
     for line in block.splitlines():
         text = line[2:]
-        leafnode_list.append(LeafNode("li", text))
+        children = __get_children(text)
+        if len(children) == 1 and children[0].tag == None:
+            leafnode_list.append(LeafNode("li", text))
+        else:
+            leafnode_list.append(HTMLNode("li", children=children))
     return HTMLNode("ul", children=leafnode_list)
 
 def ordlist_block_to_HTMLNode(block):
     leafnode_list = []
     for line in block.splitlines():
-        text = line[len(line.split()[0]):]
-        leafnode_list.append(LeafNode("li", text))
+        text = line[len(line.split()[0])+1:]
+        children = __get_children(text)
+        if len(children) == 1 and children[0].tag == None:
+            leafnode_list.append(LeafNode("li", text))
+        else:
+            leafnode_list.append(HTMLNode("li", children=children))
     return HTMLNode("ol", children=leafnode_list)
 
 def paragraph_block_to_HTMLNode(block):
-    leafnode_list = []
-    nodes = text_to_text_nodes(block)
-    for node in nodes:
-        leafnode_list.append(text_to_node_html_node(node))
+    leafnode_list = __get_children(block)
     if len(leafnode_list) == 1:
         return leafnode_list[0]
     return HTMLNode("p", children=leafnode_list)
